2f8477ebe05a98c37c4f7135b8f8c80340571780
mohanjj <mohanjaws@gmail.com>
2018-05-09 03:44:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tittu/tittu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.tittu.tittu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
